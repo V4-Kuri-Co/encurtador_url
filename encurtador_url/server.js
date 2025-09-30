16b2155f9c9947e296086c207ac0a21a008809b3
Eduardo <eduardo.henrique@franquia.com.br>
2025-09-30 10:26:33 -0300

    Update server.js

--- encurtador_url/server.js
+++ encurtador_url/server.js
@@ -55,5 +55,6 @@ app.get("/:shortUrl", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
